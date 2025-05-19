3163825d49cee7ec1957a8a4525df33004668849
anitaokoh <sleekymeeratura@gmail.com>
2025-05-19 20:06:09 +0200

    chore: add linting and commit validation setup

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier', // Make ESLint work with Prettier
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Not needed in newer React versions
+    'react/prop-types': 'off', // Disable prop-types validation
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
